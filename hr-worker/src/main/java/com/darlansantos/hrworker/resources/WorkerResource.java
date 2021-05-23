7cdfef9166187fb5e55078745aecf259368839a8
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2021-05-23 01:01:05 -0300

    Project hr-payroll created

--- hr-worker/src/main/java/com/darlansantos/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/darlansantos/hrworker/resources/WorkerResource.java
@@ -1,14 +1,12 @@
 package com.darlansantos.hrworker.resources;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.darlansantos.hrworker.entities.Worker;
